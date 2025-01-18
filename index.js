d1665947c4ccf712f0bc54ee1ff2470cb1f6db2a
Abdul-Rehman <a.rehman360916@gmail.com>
2025-01-18 11:10:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    module.exports = app;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('Database connection failed:', err));
